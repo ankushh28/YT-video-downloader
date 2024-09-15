6b893eff41d5f02e6510e06bf56286f45ffb5cee
Ankush Choudhary <74401816+ankushh28@users.noreply.github.com>
2024-09-16 01:39:10 +0530

    added user agent

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-from flask import Flask, render_template, request, send_file, redirect, url_for
+import requests
 from pytubefix import YouTube
+from flask import Flask, render_template, request, send_file
 import os
 
 app = Flask(__name__)
 
-# Route to home page
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# Route to handle download
 @app.route('/download', methods=['POST'])
 def download_video():
     url = request.form['url']
     
-    try:
-        yt = YouTube(url,use_po_token=True)
-        stream = yt.streams.get_highest_resolution()
-
-        # Download the video to a temporary directory
-        download_path = stream.download(output_path='downloads/')
-        return send_file(download_path, as_attachment=True, download_name=f"{yt.title}.mp4")
+    headers = {'User-agent': 'your bot 0.1'}
     
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            yt = YouTube(url, use_po_token=True)
+            stream = yt.streams.get_highest_resolution()
+            video_file = stream.download(output_path="downloads/")
+            
+            return send_file(video_file, as_attachment=True)
+        else:
+            return f"Failed to access the video. HTTP status code: {response.status_code}"
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
 if __name__ == '__main__':
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
+    if not os.path.exists("downloads"):
+        os.mkdir("downloads")
     app.run(debug=True)
