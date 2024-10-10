51acbe2471dd9abdbfc57562d1a81a9d3f734b37
Ankush Choudhary <74401816+ankushh28@users.noreply.github.com>
2024-10-11 00:44:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -11,26 +11,7 @@ app.post('/download-video', async (req, res) => {
     try {
         const response = await axios.post(
             'https://submagic-free-tools.fly.dev/api/youtube-info',
-            { 'url': videoUrl },
-            {
-                headers: {
-                    'accept': '*/*',
-                    'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,hi;q=0.6',
-                    'cache-control': 'no-cache',
-                    'content-type': 'application/json',
-                    'origin': 'https://submagic-free-tools.fly.dev',
-                    'pragma': 'no-cache',
-                    'priority': 'u=1, i',
-                    'referer': 'https://submagic-free-tools.fly.dev/youtube-downloader',
-                    'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'empty',
-                    'sec-fetch-mode': 'cors',
-                    'sec-fetch-site': 'same-origin',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
-                }
-            }
+            { 'url': videoUrl }
         );
         console.log(response)
         const videoWithAudio = response.data.formats.find(f => f.type === 'video_with_audio');
